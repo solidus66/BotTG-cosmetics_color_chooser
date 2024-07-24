05c24093c1e4ee2915c4333e6b9aa2967fb854b4
solidus66 <66solidus@gmail.com>
2024-07-25 00:38:04 +0300

    Update webserver

--- webserver.py
+++ webserver.py
@@ -1,27 +1,20 @@
+import os
 from flask import Flask
 from threading import Thread
-import requests
-import time
 
-app = Flask('')
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "I'm alive"
 
-def run():
-    app.run(host='0.0.0.0', port=80)
 
-# def ping_server():
-#     while True:
-#         try:
-#             requests.get('http://')
-#         except requests.exceptions.RequestException:
-#             pass
-#         time.sleep(300)  # Отправлять запрос каждые 5 минут (300 секунд)
+def run():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-    # ping_thread = Thread(target=ping_server)
-    # ping_thread.start()
